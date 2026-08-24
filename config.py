128367c98cab99f710fb35bcf8b6ca73ae863ec2
Riddhi-0804 <riddhigarg114@gmail.com>
2026-08-24 12:44:17 +0530

    Set up Flask backend foundation

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "campuspay-development-secret")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_PORT = int(os.getenv("DB_PORT", "3306"))
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+    DB_NAME = os.getenv("DB_NAME", "campuspay")
